ab5551cde93d156bca78de3a1af12f5dfa9f81e2
HongBap <sonhong6701@gmail.com>
2023-04-16 23:16:35 +0700

    HongBap

---
+++ app/src/main/java/com/example/app_mobile/Activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.app_mobile.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.app_mobile.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
